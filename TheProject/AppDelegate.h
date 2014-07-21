376cb71776b1178773fec64ae9e8aeef6366a382
Fellipe Caetano <fellipe.caetano4@gmail.com>
2014-07-21 09:29:07 -0300

    Initial Commit

---
+++ TheProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TheProject
+//
+//  Created by Mac Mini on 21/07/14.
+//  Copyright (c) 2014 Dextra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
